a33a23cba1ee38f1ac8ebef47c37c362afacf135
colby works hruska <colbyhruska@gmail.com>
2022-11-06 18:25:27 -0500

    Started training code

---
+++ gan/dataloader.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+from PIL import Image
+
+data_path = "./data/"
+
+def find_data_size():
+	return len(os.listdir(data_path)) - 1 #data folder should always contain a number of images and a singe 'readme' file as a placeholder
+
+data_size = find_data_size()
+print(f"Dataset size: {data_size:,}")
+
+def get_batch(start : int, size : int):
+	batch = []
+	for i in range(start, start + size):
+		with Image.open(data_path + f"{i}.png") as im:
+			batch.append(np.array(im))
+		#	print(str(np.array(im).shape) + ", " + str(i))
+	return np.array(batch)
---
+++ gan/samples.py
@@ -0,0 +1,12 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+def generate_latent_vectors(n_dim, n_samples):
+	x = np.random.randn(n_dim * n_samples)
+	x = x.reshape(n_samples, n_dim)
+	return x
+
+def generate_fake_samples(g_model, n_dim, n_samples):
+	x_input = generate_latent_vectors(n_dim, n_samples)
+	return g_model.predict([x_input])
