Fixed FID calculating covariance along wrong axis.

gan/FID.py
@@ -3,6 +3,7 @@ from scipy.linalg import sqrtm
 from skimage.transform import resize
 from keras.applications import InceptionV3
 from keras.applications.inception_v3 import preprocess_input
+import os
 
 model = InceptionV3(include_top=False, pooling='avg', input_shape=(299,299,3))
 
@@ -13,30 +14,41 @@ def scale(imgs, shape):
 		img_list.append(new_img)
 	return np.asarray(img_list)
 
-
-def calculate_fid(images1, images2, preprocess=True):
+def find_distribution(arr, preprocess=True):
 	if preprocess:
-		images1 = images1.astype('float32')
-		images2 = images2.astype('float32')
+		arr = arr.astype('float32')
+		arr = scale(arr, (299,299,3))
+		arr = preprocess_input(arr)
 
-		images1 = scale(images1, (299,299,3))
-		images2 = scale(images2, (299,299,3))
+	distribution = model.predict(arr)
 
-		images1 = preprocess_input(images1)
-		images2 = preprocess_input(images2)
+	mu = distribution.mean(axis=0)
+	sigma = np.cov(distribution, rowvar=False)
+	return mu, sigma
 
-	distribution1 = model.predict(images1)
-	distribution2 = model.predict(images2)
-
-	mu1, mu2 = distribution1.mean(axis=0), distribution2.mean(axis=0)
-	sigma1, sigma2 = np.cov(distribution1), np.cov(distribution2)
-
+def frechet(mu1, sigma1, mu2, sigma2):
 	sum_square_diff = np.sum((mu1 - mu2) ** 2)
-	covmean = sqrtm(sigma1.dot(sigma2))
+	dot = sigma1.dot(sigma2)
+	covmean = sqrtm(dot)
 
 	if np.iscomplexobj(covmean):
 		covmean = covmean.real
 
-	fid = sum_square_diff + np.trace(sigma1 + sigma2 - 2 * covmean)
-	return fid
+	d = sum_square_diff + np.trace(sigma1 + sigma2 - 2 * covmean)
+	return d
 
+def calculate_fid(images1 : np.array, images2 : np.array, preprocess : bool = True):
+	mu1, sigma1, = find_distribution(images1, preprocess)
+	mu2, sigma2, = find_distribution(images2, preprocess)
+
+	return frechet(mu1, sigma1, mu2, sigma2)
+
+dir = os.path.dirname(__file__)
+data_mu = np.load(os.path.join(dir, 'mu.npy'))
+data_sigma = np.load(os.path.join(dir, 'sigma.npy'))
+
+def calculate_fid(images : np.array, preprocess : bool = True):
+	mu, sigma = find_distribution(images, preprocess)
+	
+	return frechet(data_mu, data_sigma, mu, sigma) 
+	

gan/dataloader.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from PIL import Image
+import numpy as np
 
 data_path = os.path.join(os.path.dirname(__file__),"data/")
 
@@ -8,13 +9,11 @@ def find_data_size():
 	return len(os.listdir(data_path)) - 1 #data folder should always contain a number of images and a singe 'readme' file as a placeholder
 
 data_size = find_data_size()
-print(f"Dataset size: {data_size:,}")
 
 def get_batch(start : int, size : int):
 	batch = []
 	for i in range(start, start + size):
 		with Image.open(data_path + f"{i}.png") as im:
 			batch.append(np.array(im) / 255)
-			print(batch[-1])
 		#	print(str(np.array(im).shape) + ", " + str(i))
 	return np.array(batch)

gan/samples.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 from PIL import Image
 import os
+from matplotlib import pyplot
 
 def seed(seed):
 	np.random.seed(seed)
@@ -18,7 +19,17 @@ def generate_fake_samples(g_model, n_dim, n_samples):
 
 def save_image(arr):
 	with Image.fromarray((arr * 255).astype(np.uint8)) as im:
-		print(arr * 255)
 		dir = os.path.join(os.path.dirname(__file__), f"./out/")
 		n = len(os.listdir(dir))
 		im.save(os.path.join(dir, f"{n}.png"))
+
+def save_plot(examples, n=4):
+	dir = os.path.join(os.path.dirname(__file__), f"./out/")
+	file = os.path.join(dir, f"{len(os.listdir(dir))}.png")
+	for i in range(n * n):
+		pyplot.subplot(n, n, 1 + i)
+		pyplot.axis('off')
+		pyplot.imshow(examples[i])
+	pyplot.savefig(file, dpi = 159)
+	pyplot.close('all')
+	pyplot.close()

gan/train.py
@@ -14,6 +14,8 @@ import FID
 import samples
 import models
 
+print(f"Dataset size: {dataloader.data_size:,}")
+
 max_epoch = 5000
 store_img_iter = 100
 display_stats_iter = 10
@@ -22,7 +24,7 @@ batch_size = 16
 latent_dim = 128
 
 def train(d_model, g_model, gan_model):
-	sample_vector = samples.generate_latent_vectors(latent_dim, 1)
+	sample_vector = samples.generate_latent_vectors(latent_dim, 16)
 
 	n_batches = int(dataloader.data_size / batch_size)
 	for epoch in range(max_epoch):
@@ -40,11 +42,11 @@ def train(d_model, g_model, gan_model):
 
 			if (batch + 1) % display_stats_iter == 0:
 				print(f"{epoch}: {batch}/{n_batches}) d_loss = {d_loss}, accuracy = {acc}, g_loss = {g_loss}")
-				print(f"FID: {FID.calculate_fid(real_images, fake_images)}")
-				im = g_model.predict(sample_vector)[0]
+				print(f"FID: {FID.calculate_fid(fake_images)}")
+				im = g_model.predict(sample_vector)
 			#	im = np.moveaxis(im, -1, 0)
 			#	print(im.shape)
-				samples.save_image(im)
+				samples.save_plot(im)
 
 models.define_models(latent_dim)
 